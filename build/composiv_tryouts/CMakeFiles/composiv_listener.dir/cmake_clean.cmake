a16d741d573d7c990c306232dac308e09624da3b
Kaan Ural <kaanurall@hotmail.com>
2023-01-11 22:14:27 +0300

    Beginning

---
+++ build/composiv_tryouts/CMakeFiles/composiv_listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/kaan/eter_ws/devel/lib/composiv_tryouts/composiv_listener"
+  "/home/kaan/eter_ws/devel/lib/composiv_tryouts/composiv_listener.pdb"
+  "CMakeFiles/composiv_listener.dir/src/composiv_listener.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/composiv_listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/composiv_tryouts/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kaan/eter_ws/src/composiv_tryouts
+# Build directory: /home/kaan/eter_ws/build/composiv_tryouts
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
