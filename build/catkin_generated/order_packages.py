9826c5fb1acc8ce01663600119d1a23b34dee7ab
knnurl <71135352+knnurl@users.noreply.github.com>
2023-01-13 15:44:06 +0300

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/kaan/eter_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/kaan/eter_ws/devel;/opt/ros/noetic'.split(';') if '/home/kaan/eter_ws/devel;/opt/ros/noetic' != '' else []
--- build/composiv_tryouts/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kaan/eter_ws/src/composiv_tryouts
-# Build directory: /home/kaan/eter_ws/build/composiv_tryouts
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
